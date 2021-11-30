c1af9e1f9b662693d726ab88a00ebae255d69116
juweinan <1399186177@qq.com>
2021-11-30 16:36:34 +0800

    use postcss-px2viewport adaptive mobile terminal

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // 使用 postcss-pxtorem 适配移动端
   plugins: {
     'postcss-pxtorem': {
       // 设计稿为尺寸为 750*1334，因此基数设为 75（根据设计稿宽度除以 10）
@@ -7,4 +8,22 @@ module.exports = {
       propList: ['*'],
     },
   },
+  // 使用 postcss-px-to-viewport 适配移动端
+  /* plugins: {
+    autoprefixer: {}, // 用来给不同的浏览器自动添加相应的前缀
+    'postcss-px-to-viewport': {
+      unitToConvert: 'px', // 要转化的单位
+      viewportWidth: 750, // UI 设计稿的宽度
+      unitPrecision: 6, // 转换后的京都，即小数点位数
+      propList: ['*'], // 指定转换的 css 属性的单位，* 代表全部 css 属性的单位都进行转换
+      viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认 vw
+      fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认 vw
+      selectorBlackList: [], // 指定不转换为视窗单位的类名
+      minPixelValue: 1, // 默认值为 1，小于或等于 1px 则不进行转换
+      mediaQuery: true, // 是否在媒体查询的 css 代码中也进行转换，默认为 false
+      replace: true, // 是否转换后直接更换属性值
+      exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
+      landscape: false, // 是否处理横屏情况
+    },
+  }, */
 };
