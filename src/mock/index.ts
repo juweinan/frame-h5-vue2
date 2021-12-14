9d12b4640cfb6ba0b99dd3ae200136233c6db4e2
juweinan <1399186177@qq.com>
2021-12-14 19:57:35 +0800

    test: 💍 windows test githooks

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,11 +1,14 @@
-function add(a: number, b: number) {
-  return a + b;
+function add    (a:number,b:number)   
+{
+  return a+b
 }
 
-const sum: number = add(1, 3);
+const sum:number=add(1,3);
 
-const sum2: number = add(2, 3);
+const sum2:number=add(2,3);
 
-console.log('hello githooks', sum);
+console.log("hello githooks",sum)
 
-console.log('hello githooks', sum2);
+console.log("hello githooks",sum2)
+
+console.log("add.vscode will success")
